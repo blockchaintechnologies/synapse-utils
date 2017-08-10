7159d42d1dd93cf6dba87f99f7a9950fcad82732
Kanchi Chandwani <kchandwani21@gmail.com>
2017-08-10 15:16:30 -0400

    TO-DO added only comments for now

---
+++ accounts/getDataFeed.js
@@ -0,0 +1,11 @@
+/*TO-DO 
+ 
+ getDataFeed() {
+   - generate the nonce, UUID, and encrypt with secret key for the subscriber
+   - create a channel to pubsub room, verfiy the UUID, nonce and secret key as someone who has purchased the datafeed
+   - decrypt the information with the cipher if its the valid/verified person
+   - display all the infromation for the subscriber once the data has been  decrypted ( The data will not be shown as decrypted if its not decrypted in the previous step. It will shows up as encrypted or not even show up at all). 
+ }
+ 
+*/
+
