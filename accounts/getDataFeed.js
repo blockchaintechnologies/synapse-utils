b3445812735f870f011c5a7e25d85b55aec6c171
Kanchi Chandwani <kchandwani21@gmail.com>
2017-08-10 15:58:47 -0400

    Slowly adding things

--- accounts/getDataFeed.js
+++ accounts/getDataFeed.js
@@ -1,9 +1,19 @@
 /*TODO 
- getDataFeed() {
    - generate the nonce, UUID, and encrypt with secret key for the subscriber
    - create a channel to pubsub room, verfiy the UUID, nonce and secret key as someone who has purchased the datafeed
    - decrypt the information with the cipher if its the valid/verified person
-   - display all the infromation for the subscriber once the data has been  decrypted ( The data will not be shown as decrypted if its not decrypted in the previous step. It will shows up as encrypted or not even show up at all). 
- } 
+   - display all the infromation for the subscriber once the data has been  decrypted ( The data will not be shown as decrypted if its not 
+     decrypted in the previous step. It will shows up as encrypted or not even show up at all). 
 */
+
+const { generateNonce} = require('a-nonce-generator');
+const nonce = generateNonce();
+
+const var uuid = require('uuid-random');
+uuid();
+
+//ecdh.generateKeys([encoding[,format]]);
+
+
+
 
